b372e437c96d0ab8ba1748b2f651f70f75a678f3
Bulat <beast8357@mail.ru>
2022-10-29 14:56:07 +0300

    Task_1

--- Task_1/triangle_area.rb
+++ Task_1/triangle_area.rb
@@ -2,5 +2,5 @@ puts "Enter the base of the triangle in meters: "
 base = gets.chomp.to_f
 puts "Enter the height of the triangle in meters: "
 height = gets.chomp.to_f
-triangle_area = 1.0 / 2.0 * base * height
+triangle_area = 0.5 * base * height
 puts "The area of the triangle is #{triangle_area} square meters."
