72f086e9fd63088af22138604037af3bcbf8c86b
Bulat <beast8357@mail.ru>
2022-11-11 17:08:50 +0300

    Task_4

--- Task_3/app.rb
+++ Task_3/app.rb
@@ -1,10 +1,10 @@
 class App
+  attr_reader :station, :routes, :trains
+
   def initialize
     @stations = {}
     @routes = {}
     @trains = {}
-    @status = "Status:"
-    @prompt = "Your input: "
   end
 
   def menu
@@ -28,7 +28,7 @@ class App
 
     loop do
       puts actions_list
-      print "#{@prompt}"
+      prompt
       input = gets.chomp.to_i
       exit if input.zero?
       case input
@@ -53,19 +53,9 @@ class App
         when 10
           move_train(:back)
         when 11
-          show_routes_names
-          puts "Enter the name of the route."
-          print "#{@prompt}"
-          route_name = gets.chomp
-          route = get_route(route_name)
-          show_route_stations(route)
+          show_route_stations
         when 12
-          show_stations_names
-          puts "Enter the name of the station."
-          print "#{@prompt}"
-          station_name = gets.chomp
-          station = get_station(station_name)
-          show_trains_at_station(station)
+          show_trains_at_station
         when 0
           puts "See you later!"
           break
@@ -75,20 +65,22 @@ class App
 
   def create_station
     puts "Enter the name of the station."
-    print "#{@prompt}"
+    prompt
     station_name = gets.chomp
     if @stations[station_name].nil?
       @stations[station_name] = Station.new(station_name)
-      puts "#{@status} #{station_name} station has been created!"
+      status 
+      puts "#{station_name} station has been created!"
     else
-      puts "#{@status} #{station_name} station already exists!"
+      status
+      puts "#{station_name} station already exists!"
     end
   end
 
   def create_train
     puts "To create a passenger train, enter 1.\n" +
          "To create a cargo train, enter 2.";
-    print "#{@prompt}"
+    prompt
     type = gets.chomp.to_i
     case type
       when 1
@@ -97,108 +89,118 @@ class App
         type = :cargo
     end
     puts "Enter the train number."
-    print "#{@prompt}"
+    prompt
     number = gets.chomp
     case type
       when :passenger
         number = "P-" + number
         if @trains[number].nil?
           @trains[number] = PassengerTrain.new(number)
-          puts "#{@status} Passenger train #{number} has been created!"
+          status
+          puts "Passenger train #{number} has been created!"
         else
-          puts "#{@status} Passenger train #{number} already exists!"
+          status
+          puts "Passenger train #{number} already exists!"
         end
       when :cargo
         number = "C-" + number
         if @trains[number].nil?
           @trains[number] = CargoTrain.new(number)
-          puts "#{@status} Cargo train #{number} has been created!"
+          status
+          puts "Cargo train #{number} has been created!"
         else
-          puts "#{@status} Cargo train #{number} already exists!"
+          status
+          puts "Cargo train #{number} already exists!"
         end
     end
   end
 
   def create_route
     if @stations.empty?
-      puts "#{@status} No stations available!"
+      status
+      puts "No stations available!"
     else
       show_stations_names
       puts "Enter the starting station name."
-      print "#{@prompt}"
+      prompt
       starting_station_name = gets.chomp
       puts "Enter the end station name."
-      print "#{@prompt}"
+      prompt
       end_station_name = gets.chomp
       starting_station = @stations[starting_station_name]
       end_station = @stations[end_station_name]
       route_name = "#{starting_station_name}-#{end_station_name}"
       @routes[route_name] = Route.new(starting_station, end_station)
-      puts "#{@status} Route <<#{route_name}>> has been created!"
+      status
+      puts "Route <<#{route_name}>> has been created!"
     end
   end
 
   def add_station
     if @routes.empty?
-      puts "#{@status} No routes available!"
+      status
+      puts "No routes available!"
     else
       show_routes_names
       puts "Enter the name of the route you'd like to add the station to."
-      print "#{@prompt}"
+      prompt
       route_name = gets.chomp
       show_stations_names
       puts "Enter the name of the station you'd like to add."
-      print "#{@prompt}"
+      prompt
       station_name = gets.chomp
       route = get_route(route_name)
       station = get_station(station_name)
       route.add_station(station)
-      puts "#{@status} #{station_name} station has been added to the route!"
+      status
+      puts "#{station_name} station has been added to the route!"
     end 
   end
 
   def remove_station
     show_routes_names
     puts "Enter the name of the route you'd like to remove the station from."
-    print "#{@prompt}"
+    prompt
     route_name = gets.chomp
     route = get_route(route_name)
-    show_route_stations(route)
+    route_stations(route)
     puts "Enter the name of the station you'd like to remove."
-    print "#{@prompt}"
+    prompt
     station_name = gets.chomp
     station = get_station(station_name)
     if route.stations.include?(station)
       route.remove_station(station)
-      puts "#{@status} #{station_name} station has been removed from the route!"
+      status
+      puts "#{station_name} station has been removed from the route!"
     end
   end
 
   def assign_route
     show_trains_numbers
     puts "Enter the number of the train you'd like to assign a route to."
-    print "#{@prompt}"
+    prompt
     number = gets.chomp
     show_routes_names
     puts "Enter the name of the route you'd like to assign to the train."
-    print "#{@prompt}"
+    prompt
     route_name = gets.chomp
     train = get_train(number)
     route = get_route(route_name)
     train.take_route(route)
-    puts "#{@status} The route <<#{route_name}>> has been assigned to the train #{number}!"
+    status
+    puts "The route <<#{route_name}>> has been assigned to the train #{number}!"
   end
 
   def add_car
     show_trains_numbers
     puts "Enter the number of the train you'd like to add a car to."
-    print "#{@prompt}"
+    prompt
     number = gets.chomp
     train = get_train(number)
     puts "What type of car would you like to create?\n" +
         "Enter 1 to make it passenger;\n" +
         "Enter 2 to make it cargo.";
-    print "#{@prompt}"
+    prompt
     type = gets.chomp.to_i
     case type
       when 1
@@ -212,14 +214,16 @@ class App
         train.add_car(car)
         car_adding_success_message
       elsif car.type == :cargo
-        puts "#{@status} Can't add cargo cars to this train!"
+        status
+        puts "Can't add cargo cars to this train!"
       end
     elsif train.type == :cargo
       if car.type == :cargo
         train.add_car(car)
         car_adding_success_message
       elsif car.type == :passenger
-        puts "#{@status} Can't add passenger cars to this train!"  
+        status
+        puts "Can't add passenger cars to this train!"  
       end
     end
   end
@@ -227,45 +231,63 @@ class App
   def unhook_car
     show_trains_numbers
     puts "Enter the number of the train you'd like to unhook a car from."
-    print "#{@prompt}"
+    prompt
     number = gets.chomp
     train = get_train(number)
     if train.cars.empty?
-      puts "#{@status} No cars to remove!"
+      status
+      puts "No cars to remove!"
     else
       train.unhook_car(train.cars.last)
-      puts "#{@status} The car has been unhooked!"
+      status
+      puts "The car has been unhooked!"
     end
   end
 
-  def show_route_stations(route)
-    stations_list = route.stations
-    puts "Current route stations:"
-    stations_list.each do |station|
-      iterator = 0
-      puts "#{iterator += 1}. #{station.name}"
-    end
+  def show_route_stations
+    show_routes_names
+    puts "Enter the name of the route."
+    prompt
+    route_name = gets.chomp
+    route = get_route(route_name)
+    route_stations(route)
   end
 
-  def show_trains_at_station(station)
-    trains_list = station.trains
-    puts "Trains at this station:"
-    trains_list.each do |train|
-      iterator = 0
-      puts "#{iterator += 1}. #{train.number}"
-    end
+  def show_trains_at_station
+    show_stations_names
+    puts "Enter the name of the station."
+    prompt
+    station_name = gets.chomp
+    station = get_station(station_name)
+    trains_at_station(station)
   end
 
 # Реализация методов ниже скрыта в целях обеспечения стабильности работы приложения
 
   private
 
+  def route_stations(route)
+    stations_list = route.stations
+    puts "Current route stations:"
+    stations_list.each.with_index(1) do |station, index|
+      puts "#{index}. #{station.name}"
+    end
+  end
+
+  def trains_at_station(station)
+    trains_list = station.trains
+    puts "Trains at this station:"
+    trains_list.each.with_index(1) do |train, index|
+      puts "#{index}. #{train.number}"
+    end
+  end
+
   def move_train(direction)
     show_trains_numbers
     case direction
       when :forward
         puts "Enter the number of the train you'd like to move forward."
-        print "#{@prompt}"
+        prompt
         number = gets.chomp
         train = get_train(number)
         if train.next_station.nil?
@@ -276,7 +298,7 @@ class App
         end
       when :back
         puts "Enter the number of the train you'd like to move back."
-        print "#{@prompt}"
+        prompt
         number = gets.chomp
         train = get_train(number)
         if train.previous_station.nil?
@@ -289,16 +311,14 @@ class App
   end
 
   def show_routes_names
-    iterator = 0
-    @routes.keys.each do |route_name|
-      puts "#{iterator += 1}. #{route_name}"
+    @routes.keys.each.with_index(1) do |route_name, index|
+      puts "#{index}. #{route_name}"
     end
   end
 
   def show_trains_numbers
-    iterator = 0
-    @trains.values.each do |train|
-      puts "#{iterator += 1}. #{train.number}"
+    @trains.values.each.with_index(1) do |train, index|
+      puts "#{index}. #{train.number}"
     end
   end
 
@@ -321,10 +341,20 @@ class App
   end
 
   def car_adding_success_message
-    puts "#{@status} The car has been successfully added to the train!"
+    status
+    puts "The car has been successfully added to the train!"
   end
 
   def arrival_message(train)
-    puts "#{@status} The train has arrived at #{train.current_station.name} station!"
+    status
+    puts "The train has arrived at #{train.current_station.name} station!"
+  end
+
+  def status
+    print "Status: "
+  end
+
+  def prompt
+    print "Your input: "
   end
 end
